b5d2d1eb0bdedb93881caee4b48c9a37b0346fb8
Marcos Alberto <m.albertoconceicao@gmail.com>
2022-06-22 07:31:58 -0300

    linhaColunasMenu

--- Menu.cs
+++ Menu.cs
@@ -9,6 +9,36 @@ namespace EditorHtml
             Console.Clear();
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Black;
+            DrawScreem();
+        }
+        public static void DrawScreem()
+        {
+            Console.Clear();
+            Separador(30);
+            LinhaColuna(10,30);
+            Separador(30);
+            }
+        public static void Separador(int separador)
+        {
+             Console.Write("+");
+
+            for(int i = 0; i <= separador; i++)
+                Console.Write("-");
+            
+            Console.Write("+");
+            Console.Write("\n");
+        }
+        public static void LinhaColuna(int linhas, int colunas)
+        {
+            for (int lines = 0; lines <= linhas; lines++)
+            {
+                Console.Write("|");
+                for(int i = 0; i <= colunas; i++)
+                    Console.Write(" ");
+                
+                Console.Write("|");
+                Console.Write("\n");
+            }
         }
     }
 }
