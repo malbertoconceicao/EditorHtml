ff276ae78147de49935b3dc777412ecd3a90b2e6
Marcos Alberto <m.albertoconceicao@gmail.com>
2022-06-22 08:15:05 -0300

    menuDeAplicacao

--- Menu.cs
+++ Menu.cs
@@ -9,7 +9,12 @@ namespace EditorHtml
             Console.Clear();
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Black;
+            
             DrawScreem();
+            WriteOption();
+            
+            var option = short.Parse(Console.ReadLine());
+            HandleMenuOption(option);
         }
         public static void DrawScreem()
         {
@@ -20,7 +25,7 @@ namespace EditorHtml
             }
         public static void Separador(int separador)
         {
-             Console.Write("+");
+            Console.Write("+");
 
             for(int i = 0; i <= separador; i++)
                 Console.Write("-");
@@ -38,6 +43,38 @@ namespace EditorHtml
                 
                 Console.Write("|");
                 Console.Write("\n");
+            }
+        }
+        public static void WriteOption()
+        {
+            Console.SetCursorPosition(3,2);
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3,3);
+            Console.WriteLine("============");
+            Console.SetCursorPosition(3,4);
+            Console.WriteLine("Selecione uma opção abaixo");
+            Console.SetCursorPosition(3,6);
+            Console.WriteLine("1 - Novo arquivo");
+            Console.SetCursorPosition(3,7);
+            Console.WriteLine("2 - Abrir arquivo");
+            Console.SetCursorPosition(3,9);
+            Console.WriteLine("0 - Sair");
+            Console.SetCursorPosition(3,10);
+            Console.Write("Opção: ");
+        
+        }
+        public static void HandleMenuOption(short option)
+        {
+            switch(option)
+            {
+                case 1: Console.WriteLine("Editor"); break;
+                case 2: Console.WriteLine("Abrir"); break;
+                case 0: 
+                {
+                    Console.Clear();
+                    System.Environment.Exit(0);
+                    break;
+                }
             }
         }
     }
